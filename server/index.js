38d48852be40a120f0458f0ea20e02cc46eeee6f
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-07-24 18:14:08 -0300

    commit

--- server/index.js
+++ server/index.js
@@ -1,29 +1,80 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
-import {createServer} from 'node:http'
+import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
-
-io.on('connection', (socket)=> {
-    console.log('a user has conected')
-
-    socket.on('disconnect', ()=> {
-        console.log('a user has disconnect')
-    })
+const io = new Server(server, {
+  connectionStateRecovery: {}
 })
+
+const db = createClient({
+  url: 'libsql://powerful-the-punisher-marcelosanchezdev.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`)
+
+io.on('connection', async (socket) => {
+  console.log('a user has connected!')
+
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected')
+  })
+
+  socket.on('chat message', async (msg) => {
+    let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+    console.log({ username })
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+        args: { msg, username }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
+  })
+
+  if (!socket.recovered) { // <- recuperase los mensajes sin conexión
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+})
+
 app.use(logger('dev'))
 
-app.get('/', (rez, res) => {
-    res.sendFile(process.cwd() + '/client/index.html')
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
-
-server.listen(port,() => {
-    console.log(`Escuchando en el puerto http://localhost:${port}`)
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
